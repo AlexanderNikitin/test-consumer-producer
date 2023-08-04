80dc5941f095b24a0731e88c137af1d27c316d9c
Alexander Nikitin <alexnik000@yandex.ru>
2023-08-04 09:08:43 +0300

    Queue implementation is added: DefaultQueue.

---
+++ src/main/java/an/test/queue/DefaultQueue.java
@@ -0,0 +1,26 @@
+package an.test.queue;
+
+import java.util.Optional;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class DefaultQueue<T> implements Queue<T> {
+    private final BlockingQueue<T> internalQueue = new LinkedBlockingQueue<>();
+
+    @Override
+    public void put(T message) throws QueueOverflowException {
+        if (!internalQueue.offer(message)) {
+            throw new QueueOverflowException();
+        }
+    }
+
+    @Override
+    public Optional<T> pull(long timeoutMillis) {
+        try {
+            return Optional.ofNullable(internalQueue.poll(timeoutMillis, TimeUnit.MILLISECONDS));
+        } catch (InterruptedException e) {
+            return Optional.empty();
+        }
+    }
+}
